363f2c0a3e2ce71a45e88ad34b7ee35856f3c32b
Its_1akub <jacobspernoga@gmail.com>
2024-03-12 19:09:26 +0100

    - creating a class - creating an introductory environment for the game

---
+++ src/IntroWindow.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class IntroWindow extends JPanel {
+   JButton playButton;
+
+    public IntroWindow() {
+        setBackground(Color.BLUE);
+        playButton = new JButton("Play");
+
+        setLayout(new BorderLayout());
+        add(playButton, BorderLayout.CENTER);
+    }
+    public JButton getPlayButton() {
+        return playButton;
+    }
+}

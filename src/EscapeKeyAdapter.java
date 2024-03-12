bd4e472e756f0de165743af19bb6c3a3011a217f
Its_1akub <jacobspernoga@gmail.com>
2024-03-12 19:04:37 +0100

    - creating a class - adding a keylistener that opens the pause menu when esc is pressed

---
+++ src/EscapeKeyAdapter.java
@@ -0,0 +1,27 @@
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class EscapeKeyAdapter extends KeyAdapter {
+    StopMenu stopMenu;
+    boolean stopMenuShown;
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            stopMenuShown = !stopMenuShown;
+            stopMenu.setVisible(stopMenuShown);
+            System.out.println(e.getKeyChar());
+            stopMenu.revalidate();
+            stopMenu.repaint();
+
+        }
+    }
+    public void getStopMenu(StopMenu stopMenu) {
+        this.stopMenu = stopMenu;
+    }
+
+    public void getStopMenuShown(boolean stopMenuShown) {
+       this.stopMenuShown = stopMenuShown;
+    }
+
+
+}

2ef57ac4a034cee063160811bca103635bd32f2f
Its_1akub <jacobspernoga@gmail.com>
2024-03-12 19:02:57 +0100

    - creation of a class

---
+++ src/StopMenu.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class StopMenu extends JPanel {
+
+    public StopMenu() {
+        setBackground(Color.ORANGE);
+        JLabel label = new JLabel("Hra skončila");
+        add(label);
+    }
+}

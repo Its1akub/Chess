0db85aff838d71878d9a5b8acc22361c78391ea2
Its_1akub <jacobspernoga@gmail.com>
2024-03-12 19:06:21 +0100

    - creating a class - creating a server to play on the local network

---
+++ src/Server.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private int port;
+
+    public Server() {
+        try {
+            serverSocket = new ServerSocket(0);
+            port = serverSocket.getLocalPort();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void start() {
+        new Thread(() -> {
+            try {
+                clientSocket = serverSocket.accept();
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String greeting = in.readLine();
+                if ("hello server".equals(greeting)) {
+                    out.println("hello client");
+                } else {
+                    out.println("unrecognized greeting");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+    }
+
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+
+    public int getPort() {
+        return port;
+    }
+}

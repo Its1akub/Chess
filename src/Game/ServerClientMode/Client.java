40fac0602cbdf685734a059144b102298f817b42
Its_1akub <jacobspernoga@gmail.com>
2024-03-13 18:40:43 +0100

    - creating a class - creating methods to connect, disconnect and send a message

---
+++ src/Game/ServerClientMode/Client.java
@@ -0,0 +1,35 @@
+package Game.ServerClientMode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void startConnection(int port)  {
+        try {
+            clientSocket = new Socket("localhost", port);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String sendMessage(String msg) throws IOException {
+        out.println(msg);
+        String resp = in.readLine();
+        return resp;
+    }
+
+    public void stopConnection() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+    }
+}

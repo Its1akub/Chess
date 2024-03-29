ad89541ef8e47b6c04db7c35ab2c3706849481d0
Its_1akub <jacobspernoga@gmail.com>
2024-03-29 15:25:50 +0100

    creation of a player

--- src/Other/Users/Player.java
+++ src/Other/Users/Player.java
@@ -1,4 +1,24 @@
 package Other.Users;
 
-public class Player {
+import java.io.Serializable;
+
+public class Player implements Serializable {
+    private ColorSide color;
+    private String name;
+
+    public ColorSide getColor() {
+        return color;
+    }
+
+    public void setColor(ColorSide color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }

63ebf1e9f1f0c3fc931a562e1fe95a47212bb5a9
Its_1akub <jacobspernoga@gmail.com>
2024-03-29 15:21:35 +0100

    when the client is connected jpanel updates to chess

--- src/Game/ServerClientMode/ClientPanel.java
+++ src/Game/ServerClientMode/ClientPanel.java
@@ -1,29 +1,43 @@
 package Game.ServerClientMode;
 
+import Game.Chess;
+import Other.ChessPieces.Piece;
+
 import javax.swing.*;
 import java.awt.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class ClientPanel extends JPanel {
-    public ClientPanel() {
+    private Chess chess;
+    private RunnableClient runnableClient;
+    private ArrayList<Piece> pieces = new ArrayList<>();
+    JLabel portLabel;
+    JTextArea portArea;
+    JButton connectButton;
+
+    public ClientPanel(int width, int height) {
         setBackground(Color.GRAY);
         setLayout(new BorderLayout());
-        JLabel portLabel = new JLabel("Enter port: ");
+        chess = new Chess(width, height);
+        add(chess, BorderLayout.CENTER);
+        chess.setVisible(false);
+
+        portLabel = new JLabel("Enter port: ");
         add(portLabel, BorderLayout.NORTH);
-        JTextArea portArea = new JTextArea(1, 6);
+        portArea = new JTextArea(1, 6);
         add(portArea, BorderLayout.SOUTH);
-        JButton connectButton = new JButton("Connect");
+        connectButton = new JButton("Connect");
         add(connectButton, BorderLayout.EAST);
+
         connectButton.addActionListener(e -> {
             if (portArea.getText().length() <= 6) {
                 try {
-                    Socket socket = new Socket("localhost", Integer.parseInt(portArea.getText()));
-                    System.out.println("Connecting to port... " + portArea.getText());
-                    socket.close();
-
                     int port = Integer.parseInt(portArea.getText());
-                    Client client = new Client();
-                    client.startConnection(port);
+                    System.out.println("Connecting to port... " + port);
+                    runnableClient = new RunnableClient(port);
+                    Thread thread = new Thread(runnableClient);
+                    thread.start();
                 } catch (Exception ex) {
                     System.out.println("Couldn't connect to 'localhost' on port: " + portArea.getText());
                 }
@@ -31,9 +45,20 @@ public class ClientPanel extends JPanel {
             } else {
                 JOptionPane.showMessageDialog(null, "Enter a valid port number", "Error", JOptionPane.ERROR_MESSAGE);
             }
-
-
         });
 
+        Timer timer = new Timer(2500, null);
+        timer.addActionListener(e -> {
+            if (runnableClient != null && runnableClient.getClient() != null && runnableClient.getClient().isRunChess()) {
+                remove(portLabel);
+                remove(portArea);
+                remove(connectButton);
+                chess.setVisible(true);
+                revalidate();
+                repaint();
+                timer.stop();
+            }
+        });
+        timer.start();
     }
 }

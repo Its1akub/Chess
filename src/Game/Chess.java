be5e718c09290059445afedddf31bb542e8bd4d8
Its_1akub <jacobspernoga@gmail.com>
2024-04-04 23:24:38 +0200

    updated

--- src/Game/Chess.java
+++ src/Game/Chess.java
@@ -15,18 +15,20 @@ public class Chess extends JPanel {
     private Player p2;
 
 
-    public Chess(int width, int height) {
+    public Chess(int width, int height,boolean isWhite) {
         setPreferredSize(new Dimension(width, height));
         setLayout(new BorderLayout());
         stopMenu = new StopMenu(width, height);
         stopMenu.setVisible(false);
         add(stopMenu);
 
-        chessBoard = new ChessBoard(width, height);
+        chessBoard = new ChessBoard(width, height, isWhite);
 
         add(chessBoard,BorderLayout.CENTER);
 
 
-
+    }
+    public ChessBoard getChessBoard() {
+        return chessBoard;
     }
 }

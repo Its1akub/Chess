d96257ff0d4a8940e1e0c04257d341abe420356c
Its_1akub <jacobspernoga@gmail.com>
2024-04-05 17:50:56 +0200

    added promotion of pawn

--- src/Game/Mechanics.java
+++ src/Game/Mechanics.java
@@ -1,10 +1,13 @@
 package Game;
 
+import Other.ChessPieces.Pawn;
 import Other.ChessPieces.Piece;
+import Other.ChessPieces.Queen;
 import Other.Users.ColorSide;
 import Other.Users.Player;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 
 import static Other.Users.ColorSide.BLACK;
@@ -35,7 +38,7 @@ public class Mechanics {
                         String color;
                         if (p.isWhite()) {
                             color = "w";
-                        }else {
+                        } else {
                             color = "b";
                         }
                         String type = switch (p.getType()) {
@@ -43,13 +46,13 @@ public class Mechanics {
                                 yield "k";
                             case QUEEN:
                                 yield "q";
-                            case BISHOP :
+                            case BISHOP:
                                 yield "b";
-                            case KNIGHT :
+                            case KNIGHT:
                                 yield "n";
-                            case ROOK :
+                            case ROOK:
                                 yield "r";
-                            case PAWN :
+                            case PAWN:
                                 yield "p";
                         };
                         board[y][x] = type + color;
@@ -59,25 +62,28 @@ public class Mechanics {
         }
         return board;
     }
+
     private static String getColor(String[][] board, int x, int y, boolean isWhite) {
         char[] color = board[y][x].toCharArray();
         if (!isWhite && color[1] == 'w') {
             return "w";
-        } else if(isWhite && color[1] == 'b'){
+        } else if (isWhite && color[1] == 'b') {
             return "b";
-        }else {
+        } else {
             return "";
         }
     }
-    public static boolean possMoveToKill(String[][] board, int x, int y, boolean isWhite){
-        if (x>7 || x<0 || y>7 || y<0){
+
+    public static boolean possMoveToKill(String[][] board, int x, int y, boolean isWhite) {
+        if (x > 7 || x < 0 || y > 7 || y < 0) {
             return false;
-        } else if (isWhite){
+        } else if (isWhite) {
             return getColor(board, x, y, true).equals("b");
-        }else {
+        } else {
             return getColor(board, x, y, false).equals("w");
         }
     }
+
     public static ArrayList<Piece> removePiece(ArrayList<Piece> pieces, int x, int y) {
         for (Piece p : pieces) {
             if (p.getcX() == x && p.getcY() == y) {
@@ -87,4 +93,23 @@ public class Mechanics {
         }
         return pieces;
     }
+
+    public static ArrayList<Piece> promotion(ArrayList<Piece> pieces, int xMoveBy, int yMoveBy) {
+        // only promoted to queen
+        for (Piece p : pieces) {
+            if (p instanceof Pawn && p.getcY() == 0) {
+                int x = p.getcX();
+                int y = p.getcY();
+                pieces.remove(p);
+                Queen queen = new Queen(x, y, p.isWhite());
+                queen.setX(x * 50 * 2 + xMoveBy + 50);
+                queen.setY(y * 50 * 2 + yMoveBy + 50);
+                pieces.add(queen);
+                break;
+            }
+        }
+        return pieces;
+    }
+
+    // public static Piece Castling
 }

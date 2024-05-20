ade81d579620991d7be7c5f7ff01fe9dfb5cb84f
Its_1akub <jacobspernoga@gmail.com>
2024-05-20 21:15:28 +0200

    added docs

--- src/Game/MovePanel.java
+++ src/Game/MovePanel.java
@@ -22,17 +22,35 @@ public class MovePanel extends JPanel {
         scrollPane.setBorder(null);
         add(scrollPane);
     }
+
+    /**
+     * Adds a move to the text area and updates the display.
+     *
+     * @param  line  the move to be added
+     */
     public void addMove(String line) {
         textArea.append(line+"      ");
         repaint();
         revalidate();
     }
+
+    /**
+     * Appends a new line to the text area, increments the line number, and updates the display.
+     *
+     * This function appends a newline character to the end of the text area, increments the line number,
+     * and appends the line number followed by three spaces to the text area. It then triggers a repaint
+     * and revalidation of the component to update the display.
+     */
     public void newLine() {
         textArea.append("\n");
         textArea.append(lineNumber+ ".      ");
         repaint();
         revalidate();
     }
+
+    /**
+     * Prints the contents of the text area to the console.
+     */
     public void print() {
         System.out.println(textArea.getText());
     }

6e5a802d5feb793c18b7acde032101927fb2a445
Its_1akub <jacobspernoga@gmail.com>
2024-05-23 18:58:52 +0200

    fix multiplayer

--- src/Game/ServerClientMode/ClientPanel.java
+++ src/Game/ServerClientMode/ClientPanel.java
@@ -1,21 +1,34 @@
 package Game.ServerClientMode;
 
 import Game.Chess;
+import Game.Move;
 import Other.ChessPieces.Piece;
 import Other.Users.ColorSide;
+import Other.Users.Player;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static Game.Mechanics.addPieceAfterMove;
+import static Game.ServerClientMode.MechanicsForMultiplayer.convertMoveToSendMove;
 
 public class ClientPanel extends JPanel {
     private Chess chess;
-    private RunnableClient runnableClient;
     private ArrayList<Piece> pieces = new ArrayList<>();
-    JLabel portLabel;
-    JTextArea portArea;
-    JButton connectButton;
+    private JLabel portLabel;
+    private JTextArea portArea;
+    private JButton connectButton;
+    private Socket clientSocket;
+    private ObjectOutputStream oos;
+    private ObjectInputStream ois;
+    private InputStream is;
+    private OutputStream os;
+    private Player pGuest, pHost;
+    private SendMove moveHost, moveGuest;
 
     public ClientPanel(int width, int height) {
         setBackground(Color.GRAY);
@@ -27,15 +40,17 @@ public class ClientPanel extends JPanel {
         add(portArea, BorderLayout.SOUTH);
         connectButton = new JButton("Connect");
         add(connectButton, BorderLayout.EAST);
-
+        AtomicInteger port = new AtomicInteger();
+        final Object lock = new Object();
         connectButton.addActionListener(e -> {
             if (portArea.getText().length() <= 6) {
                 try {
-                    int port = Integer.parseInt(portArea.getText());
-                    System.out.println("Connecting to port... " + port);
-                    runnableClient = new RunnableClient(port);
-                    Thread thread = new Thread(runnableClient);
-                    thread.start();
+                    port.set(Integer.parseInt(portArea.getText()));
+                    synchronized (lock) {
+                        lock.notify();
+                        System.out.println("Button pressed, ClientPanel notified.");
+                    }
+                    System.out.println("Connecting to port... " + port.get());
                 } catch (Exception ex) {
                     System.out.println("Couldn't connect to 'localhost' on port: " + portArea.getText());
                 }
@@ -44,21 +59,91 @@ public class ClientPanel extends JPanel {
                 JOptionPane.showMessageDialog(null, "Enter a valid port number", "Error", JOptionPane.ERROR_MESSAGE);
             }
         });
+        Thread thread = new Thread(() -> {
+            synchronized (lock) {
+                try {
+                    System.out.println("ClientPanel waiting...");
+                    lock.wait();  // Wait until notified
+                    System.out.println("ClientPanel notified!");
 
-        Timer timer = new Timer(2500, null);
-        timer.addActionListener(e -> {
-            if (runnableClient != null && runnableClient.getClient() != null && runnableClient.getClient().isRunChess()) {
-                remove(portLabel);
-                remove(portArea);
-                remove(connectButton);
-                chess = new Chess(width, height,runnableClient.getClient().getpHost().getColor() == ColorSide.BLACK);
-                add(chess, BorderLayout.CENTER);
-                chess.setVisible(true);
-                revalidate();
-                repaint();
-                timer.stop();
+                    try {
+                        clientSocket = new Socket("localhost", port.get());
+                        is = clientSocket.getInputStream();
+                        os = clientSocket.getOutputStream();
+                        oos = new ObjectOutputStream(os);
+                        ois = new ObjectInputStream(is);
+                        pGuest = (Player) ois.readObject();
+                        pHost = (Player) ois.readObject();
+
+                        remove(portLabel);
+                        remove(portArea);
+                        remove(connectButton);
+                        //pHost.getColor() == ColorSide.BLACK
+                        chess = new Chess(width, height,true);
+                        add(chess, BorderLayout.CENTER);
+                        chess.setVisible(true);
+                        revalidate();
+                        repaint();
+                        pieces = chess.getChessBoard().getPieces();
+
+                        while (true) {
+                            if (pHost.getColor() == ColorSide.WHITE) {
+                                chess.getChessBoard().setMadeMove(true);
+                                moveHost = (SendMove) ois.readObject();
+                                printHostMove(moveHost);
+                                System.out.println("Received move: " + moveHost);
+                                chess.getChessBoard().setMadeMove(false);
+                                guestMoveMethod();
+                            } else {
+                                guestMoveMethod();
+                                moveHost = (SendMove) ois.readObject();
+                                System.out.println("Received move: " + moveHost);
+                                printHostMove(moveHost);
+                                chess.getChessBoard().setMadeMove(false);
+                            }
+                        }
+                    } catch (IOException | ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
-        timer.start();
+        thread.start();
+    }
+
+    private void guestMoveMethod() throws IOException {
+        while (!chess.getChessBoard().isMadeMove()) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        pieces = chess.getChessBoard().getPieces();
+        moveGuest = convertMoveToSendMove(chess.getChessBoard().getLastMove());
+        System.out.println("sent move: " + moveGuest);
+        SendMove mgRotate = moveGuest;
+        mgRotate.rotateCoordinates();
+
+        oos.writeObject(mgRotate);
+        oos.flush();
+        moveGuest = null;
+
+    }
+
+    private void printHostMove(SendMove moveHost) {
+        //ArrayList<Piece> piecesAfterMove = pieces;
+        for (Piece p : pieces) {
+            if (moveHost != null && p.getcX() == moveHost.getPreviousCX() && p.getcY() == moveHost.getPreviousCY()) {
+                //piecesAfterMove.remove(p);
+                //addPieceAfterMove(piecesAfterMove, p, moveHost.getCurrentCX(), moveHost.getCurrentCY());
+                p.setcX(moveHost.getCurrentCX());
+                p.setcY(moveHost.getCurrentCY());
+            }
+        }
+        chess.getChessBoard().setPieces(pieces);
     }
 }
--- src/Game/ServerClientMode/ServerPanel.java
+++ src/Game/ServerClientMode/ServerPanel.java
@@ -2,58 +2,142 @@ package Game.ServerClientMode;
 
 import Game.Chess;
 import Game.Mechanics;
+import Game.Move;
 import Game.PiecesLoad;
 import Game.ServerClientMode.Server;
 import Other.ChessPieces.Piece;
+import Other.Coordinates;
 import Other.Users.ColorSide;
+import Other.Users.Player;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
+
+import static Game.Mechanics.addPieceAfterMove;
+import static Game.ServerClientMode.MechanicsForMultiplayer.convertMoveToSendMove;
 
 
 public class ServerPanel extends JPanel {
     //private Server server;
     private ArrayList<Piece> pieces = new ArrayList<>();
     private Chess chess;
-    private RunnableServer runnableServer;
-    Label serverPort;
-    Label waitingForClient;
+    private Label serverPort;
+    private Label waitingForClient;
+    private ServerSocket serverSocket;
+
+    private Socket clientSocket;
+    private ObjectInputStream ois;
+    private ObjectOutputStream oos;
+    private InputStream is;
+    private OutputStream os;
+    private Player pHost, pGuest;
+    private boolean runChess;
+    private SendMove moveHost, moveGuest;
+
     public ServerPanel(int width, int height) {
         setBackground(Color.YELLOW);
         setLayout(new BorderLayout());
+        try {
+            serverSocket = new ServerSocket(0);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-        runnableServer = new RunnableServer();
-        Thread thread = new Thread(runnableServer);
+        final int[] port = new int[1];
+        port[0] = serverSocket.getLocalPort();
 
-        thread.start();
-        int port = runnableServer.getServer().getPort();
-
-
-        //server = new Server();
-        //int port = server.getPort();
-         serverPort = new Label("Server Port: " + port);
-        System.out.println(port);
-         waitingForClient = new Label("Waiting for client...");
+        serverPort = new Label("Server Port: " + port[0]);
+        System.out.println(port[0]);
+        waitingForClient = new Label("Waiting for client...");
         add(serverPort, BorderLayout.NORTH);
         add(waitingForClient, BorderLayout.SOUTH);
 
-        Timer timer = new Timer(2500,null);
-        timer.addActionListener(e -> {
-            if (runnableServer != null && runnableServer.getServer() != null && runnableServer.getServer().isRunChess()) {
+        Thread thread = new Thread(() -> {
+            try {
 
+                ArrayList<Player> colors = Mechanics.setColorsOfPlayers();
+                pHost = colors.get(0);
+                pGuest = colors.get(1);
+                clientSocket = serverSocket.accept();
+                System.out.println(clientSocket);
                 remove(serverPort);
                 remove(waitingForClient);
-                chess = new Chess(width, height,runnableServer.getServer().getpHost().getColor() == ColorSide.WHITE);
+                //pHost.getColor() == ColorSide.WHITE
+                chess = new Chess(width, height, false);
                 add(chess, BorderLayout.CENTER);
                 chess.setVisible(true);
                 revalidate();
                 repaint();
-                timer.stop();
-            }
-        });
-        timer.start();
+                pieces = chess.getChessBoard().getPieces();
 
+                is = clientSocket.getInputStream();
+                os = clientSocket.getOutputStream();
+                ois = new ObjectInputStream(is);
+                oos = new ObjectOutputStream(os);
+                oos.writeObject(pGuest);
+                oos.writeObject(pHost);
+                while (true) {
+
+                    if (pHost.getColor() == ColorSide.WHITE) {
+                        hostMoveMethod();
+                        moveGuest = (SendMove) ois.readObject();
+                        System.out.println("Received move: " + moveGuest);
+                        printGuestMove(moveGuest);
+                        chess.getChessBoard().setMadeMove(false);
+                    } else {
+                        chess.getChessBoard().setMadeMove(true);
+                        moveGuest = (SendMove) ois.readObject();
+                        printGuestMove(moveGuest);
+                        System.out.println("Received move: " + moveGuest);
+                        chess.getChessBoard().setMadeMove(false);
+                        hostMoveMethod();
+                    }
+                }
+            } catch (IOException | ClassNotFoundException | NullPointerException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+        thread.start();
+    }
+
+    private void hostMoveMethod() throws IOException {
+        while (!chess.getChessBoard().isMadeMove()) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+
+        pieces = chess.getChessBoard().getPieces();
+        moveHost = convertMoveToSendMove(chess.getChessBoard().getLastMove());
+        System.out.println("sent move: " + moveHost);
+        SendMove mhRotate = moveHost;
+        mhRotate.rotateCoordinates();
+
+        oos.writeObject(mhRotate);
+        oos.flush();
+        moveHost = null;
+
+    }
+
+    private void printGuestMove(SendMove moveGuest) {
+        //ArrayList<Piece> piecesAfterMove = pieces;
+        int i = 0;
+        for (Piece p : pieces) {
+            if (moveGuest != null && p.getcX() == moveGuest.getPreviousCX() && p.getcY() == moveGuest.getPreviousCY()) {
+                p.setcX(moveGuest.getCurrentCX());
+                p.setcY(moveGuest.getCurrentCY());
+                //piecesAfterMove.add(i,);
+                //addPieceAfterMove(piecesAfterMove, p, moveGuest.getCurrentCX(), moveGuest.getCurrentCY());
+            }
+            i++;
+        }
+        chess.getChessBoard().setPieces(pieces);
     }
 }

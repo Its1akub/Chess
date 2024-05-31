01dcae803c3af68dbdca623c9aa782c9cf7ee6b8
Its_1akub <jacobspernoga@gmail.com>
2024-05-31 23:02:27 +0200

    added javadocs

--- src/Game/ServerClientMode/MechanicsForMultiplayer.java
+++ src/Game/ServerClientMode/MechanicsForMultiplayer.java
@@ -1,12 +1,26 @@
 package Game.ServerClientMode;
 
 import Game.Move;
-import Other.ChessPieces.*;
-
-import java.util.ArrayList;
 
 public class MechanicsForMultiplayer {
+
+    /**
+     * Converts a Move object to a SendMove object.
+     *
+     * @param  move  the Move object to convert
+     * @return       a new SendMove object with the same coordinates as the input Move object
+     */
     public static SendMove convertMoveToSendMove(Move move) {
         return new SendMove(move.getPreviousCX(), move.getPreviousCY(), move.getCurrentCX(), move.getCurrentCY());
+    }
+
+    /**
+     * Converts a SendMove object to a Move object.
+     *
+     * @param  move  the SendMove object to convert
+     * @return       a new Move object with the same coordinates as the input SendMove object
+     */
+    public static Move convertSendMoveToMove(SendMove move) {
+        return new Move(move.getPreviousCX(), move.getPreviousCY(), null,move.getCurrentCX(), move.getCurrentCY());
     }
 }

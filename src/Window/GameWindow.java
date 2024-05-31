7bfc2d9364145fde9f2f275cbbfaabadfb7e4bc7
Its_1akub <jacobspernoga@gmail.com>
2024-05-31 23:08:27 +0200

    added javadocs

--- src/Window/GameWindow.java
+++ src/Window/GameWindow.java
@@ -1,24 +1,18 @@
 package Window;
 
 import Game.AI.MyAIPanel;
-import Game.Chess;
-import Game.AI.MyAI;
-import Game.MyAIModeNotWorking.RunMyAINotWorking;
 import Game.ServerClientMode.ClientPanel;
 import Game.ServerClientMode.ServerPanel;
 import Game.StockFishMode.StockFishPanel;
-import Other.EscapeKeyAdapter;
-import Other.Users.ColorSide;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class GameWindow extends JFrame {
     IntroWindow introWindow = new IntroWindow();
-    EscapeKeyAdapter escapeKeyAdapter = new EscapeKeyAdapter();
     SelectingGame selectingGame = new SelectingGame();
     //RunMyAINotWorking runMyAINotWorking;
-    //MyAIPanel m;
+    MyAIPanel m;
     StockFishPanel stockfish;
     ServerPanel server;
     ClientPanel clientPanel;
@@ -28,12 +22,9 @@ public class GameWindow extends JFrame {
     private int width, height;
 
     public GameWindow() {
-        exitButton = new JButton("Exit");
         setPreferredSize(new Dimension());
-        //getContentPane().add(exitButton);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //setUndecorated(true);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         if (isUndecorated()) {
             width = (int) screenSize.getWidth();
@@ -47,14 +38,17 @@ public class GameWindow extends JFrame {
 
         setFocusable(true);
         requestFocusInWindow();
-        addKeyListener(escapeKeyAdapter);
         add(introWindow, BorderLayout.CENTER);
         setVisible(true);
-        escapeKeyAdapter.getStopMenu(Chess.stopMenu, Chess.stopMenuShown);
         setButtons();
 
     }
 
+    /**
+     * Switches the current panel to the specified new panel.
+     *
+     * @param  newPanel  the new panel to switch to
+     */
     public void switchToPanel(JPanel newPanel) {
         if (currentPanel != null) {
             remove(currentPanel);
@@ -65,12 +59,19 @@ public class GameWindow extends JFrame {
         repaint();
     }
 
+    /**
+     * Sets up the action listeners for the buttons in the game window.
+     * This method adds action listeners to the exit button, play button, local host button,
+     * local join button, stockfish button, and my AI button. When each button is clicked,
+     * the corresponding action is performed.
+     *
+     */
     private void setButtons() {
-        exitButton.addActionListener(e -> System.exit(0));
+        //exitButton.addActionListener(e -> System.exit(0));
         introWindow.playButton.addActionListener(e -> switchToPanel(selectingGame));
         selectingGame.localHostButton.addActionListener(e -> switchToPanel(server = new ServerPanel(width, height)));
         selectingGame.localJoinButton.addActionListener(e -> switchToPanel(clientPanel = new ClientPanel(width, height)));
         selectingGame.stockfishButton.addActionListener(e -> switchToPanel(stockfish = new StockFishPanel(width, height)));
-        //selectingGame.myAIButton.addActionListener(e -> switchToPanel(m = new MyAIPanel(width, height)));
+        selectingGame.myAIButton.addActionListener(e -> switchToPanel(m = new MyAIPanel(width, height)));
     }
 }

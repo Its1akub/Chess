385789122663fb0f4ecac2b749b9c6bf5c316122
Its_1akub <jacobspernoga@gmail.com>
2024-05-31 23:03:46 +0200

    added javadocs, method for converting object and for converting move to the board

--- src/Game/AI/MechanicsForMyAI.java
+++ src/Game/AI/MechanicsForMyAI.java
@@ -1,7 +1,17 @@
 package Game.AI;
 
+import Game.Move;
+import Other.ChessPieces.*;
+import java.util.ArrayList;
+
 public class MechanicsForMyAI {
 
+    /**
+     * Converts a FEN string representation of a chess board into a 2D array representation.
+     *
+     * @param  fen   the FEN string representation of the chess board
+     * @return       a 2D array representation of the chess board
+     */
     public static String[][] fenToBoard(String fen) {
         String[] parts = fen.split(" ");
         String[] rows = parts[0].split("/");
@@ -21,4 +31,46 @@ public class MechanicsForMyAI {
         }
         return board;
     }
+
+    /**
+     * Converts a move made by the AI in the board representation to the actual board representation.
+     *
+     * @param  pieces  the current state of the board represented as a list of pieces
+     * @param  move   the move made by the AI in the board representation
+     * @return        the updated board representation after applying the AI move
+     */
+    public static ArrayList<Piece> convertMyAIMoveToBoard(ArrayList<Piece> pieces, BoardMove move) {
+        ArrayList<Piece> piecesToRemove = new ArrayList<>();
+        for (Piece piece : pieces) {
+            if (piece.getcX() == move.getStartX() && piece.getcY() == move.getStartY()) {
+                if (piece.getcX() == move.getEndX() && piece.getcY() == move.getEndY()) {
+                    piecesToRemove.add(piece);
+                } else {
+                    piece.setcX(move.getEndX());
+                    piece.setcY(move.getEndY());
+                    break;
+                }
+
+            }
+        }
+        pieces.removeAll(piecesToRemove);
+        return pieces;
+    }
+
+    /**
+     * Generates a Move object based on the given BoardMove object.
+     *
+     * @param  move  the BoardMove object containing the start and end coordinates of the move
+     * @return       a Move object representing the move with the AI's piece's starting coordinates,
+     *               null as the captured piece, and the opponent's piece's ending coordinates
+     */
+    public static Move myAIMove(BoardMove move) {
+        int pPX = move.getStartX();
+        int pPY = move.getStartY();
+        int cPX = move.getEndX();
+        int cPY = move.getEndY();
+        return new Move(pPX, pPY, null, cPX, cPY);
+    }
+
+
 }
